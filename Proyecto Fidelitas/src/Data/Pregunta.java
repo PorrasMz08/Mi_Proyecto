81cc0c2bb2623dbf70cb3dd54ffcd8df6b07f22b
PorrasMz08 <porrasmz080@gmail.com>
2022-04-06 19:03:06 +0200

    Commit Inicial

---
+++ Proyecto Fidelitas/src/Data/Pregunta.java
@@ -0,0 +1,42 @@
+package Data;
+
+public class Pregunta {
+    String pregunta1;
+    String pregunta2;
+    String pregunta3;
+    String pregunta4;
+    String pregunta5;
+
+
+    public void Pregunta() {
+        this.pregunta1 = pregunta1;
+        this.pregunta2 = pregunta2;
+        this.pregunta3 = pregunta3;
+        this.pregunta4 = pregunta4;
+        this.pregunta5 = pregunta5;
+
+    }
+    public String getpregunta1() {
+        return pregunta1;
+    }
+    public String getpregunta2() {
+        return pregunta2;
+    }
+    public String getpregunta3() {
+        return pregunta3;  }
+    public String getpregunta4() {
+        return pregunta4;
+    }
+    public String getpregunta5() {return pregunta5;  }
+
+
+
+
+    public void setpregunta1(String pregunta1) {this.pregunta1 = pregunta1;}
+    public void setpregunta2(String pregunta2) {this.pregunta1 = pregunta2;}
+    public void setpregunta3(String pregunta3) {this.pregunta1 = pregunta3;}
+    public void setpregunta4(String pregunta4) {this.pregunta1 = pregunta4;}
+    public void setpregunta5(String pregunta5) {this.pregunta1 = pregunta5;}
+
+}
+
---
+++ Proyecto Fidelitas/src/Data/Salario.java
@@ -0,0 +1,20 @@
+package Data;
+
+public class Salario {
+    int salariobruto;
+
+
+    public Salario() {
+        this.salariobruto = salariobruto;
+
+    }
+    public int getSalariobruto() {
+        return salariobruto;
+
+    }
+
+    public void setSalariobruto(int salariobruto) {
+        this.salariobruto = salariobruto;}
+
+}
+
